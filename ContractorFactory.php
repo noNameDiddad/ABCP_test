45e1a0b16cd3f196827bb88e94e2f71a512637b1
d.shvetsov <inspairax230801@mail.ru>
2024-06-25 14:40:59 +0500

    initial commit

---
+++ ContractorFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace NW\WebService\References\Operations\Notification;
+
+class ContractorFactory
+{
+    public static function createById(int $id): Contractor
+    {
+
+    }
+
+    public static function createSellerById(int $id): Seller
+    {
+
+    }
+
+    public static function createEmployeeById(int $id): Employee
+    {
+
+    }
+}
